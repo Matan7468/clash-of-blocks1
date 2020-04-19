1327475cab884db567373dc228f53e8493c2b9bf
Matan7468 <58522264+Matan7468@users.noreply.github.com>
2020-04-19 15:23:17 +0300

    Add files via upload

---
+++ Models/SqlHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace Clash_Of_Blocks.Droid
+{
+    class SqlHelper
+    {
+        private static SQLiteConnection connection = new SQLiteConnection(GetPath());
+        public static SQLiteConnection GetConnection()
+        {
+            return connection;
+        }
+
+        public static string GetPath()
+        {
+            string dbName = "clash_of_blocksDB20";
+            string directory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string fullPath = System.IO.Path.Combine(directory, dbName);
+            return fullPath;
+        }
+
+        public static string BitmapToBase64(Bitmap bitmap)
+        {
+            string str = "";
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Compress(Bitmap.CompressFormat.Png, 100, stream);
+
+                byte[] bytes = stream.ToArray();
+                str = Convert.ToBase64String(bytes);
+            }
+            return str;
+        }
+
+        public static Bitmap Base64ToBitmap(String base64String)
+        {
+            byte[] imageAsBytes = Base64.Decode(base64String, Base64Flags.Default);
+            return BitmapFactory.DecodeByteArray(imageAsBytes, 0, imageAsBytes.Length);
+        }
+    }
+}
